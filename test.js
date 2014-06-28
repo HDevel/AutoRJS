36615622ad594545c1c5abf1cc73ca4dc16e222a
HDevel <michailfurzikov@Mac-Fucking-Book.local>
2014-06-28 11:00:53 +0400

    update

---
+++ test.js
@@ -0,0 +1,21 @@
+﻿arjs.extend({
+    name:'test',
+    defines: function()
+    {
+
+    },
+    run: function ()
+    {
+        arjs.preload.image({
+            'testOver':'media/testOver.png',
+            'img_01':'media/img_01.png',
+            'testCrop':'media/testCrop.png'
+        });
+        arjs.preload.progress(
+            function(info)
+            {
+                console.log(info);
+            }
+        )
+    }
+});
